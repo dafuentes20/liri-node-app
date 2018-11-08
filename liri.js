d407b1a3862dc2a2bc3fd07c6abc4d9917a558ec
Alan Fountain <alanfountain@Alans-MacBook-Pro.local>
2018-11-07 18:48:53 -0700

    liri app

--- liri.js
+++ liri.js
@@ -1,1 +1,7 @@
 require("dotenv").config();
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var keys = require("./keys");
+var moment = require("moment");
+var axios = require("axios");
+var spotify = new Spotify(keys.spotify);
